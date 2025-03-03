a1c1bb7238a938571c26228103be780c4397cef8
AlyssonHendryk <alyssinhendryk48@gmail.com>
2025-03-03 14:33:04 -0300

    atualização

--- main.py
+++ main.py
@@ -1,7 +1,26 @@
 import pandas as pd
-import matplotlib.pyplot as plt; plt.rcdefaults()
-import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
 
-tabela_df = pd.read_csv("Mobiles Dataset.csv", encoding="ISO-8859-1", sep=",")
 
-modelo_df = tabela_df.loc[tabela_df['Model'] != 'Name']
+df = pd.read_csv("Mobiles Dataset.csv", encoding="ISO-8859-1")
+
+
+print(df.head())
+
+
+df_sorted = df.sort_values(by="Launched Price (USA)", ascending=False)
+
+ 
+plt.figure(figsize=(12, 6))
+sns.barplot(x=df_sorted['Company Name'], y=df_sorted['Launched Price (USA)'])
+
+
+plt.xticks(rotation=90)  
+plt.title("Preço de Lançamento por Marca")
+plt.xlabel("Marcas")
+plt.ylabel("Preço (USD)")
+plt.grid(axis="y", linestyle="--", alpha=0.7)  
+
+
+plt.show()
